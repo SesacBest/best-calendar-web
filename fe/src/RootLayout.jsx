8c34a87e63c3c90e1a76bf76e329f60d5564eb60
kska01 <insideka01@gmail.com>
2025-02-23 12:31:09 +0900

    [BCW-10] feat: Route 설정 완료
    - router/index.jsx에 route 설정 완료
    - RootLayout.jsx에 Home component 설정 완료

--- fe/src/RootLayout.jsx
+++ fe/src/RootLayout.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Home from './pages/Home';
 
 export default function RootLayout() {
   return (
-    <div>RootLayout</div>
-  )
+    <>
+      <Home />
+    </>
+  );
 }
--- fe/src/router/index.jsx
+++ fe/src/router/index.jsx
@@ -1,20 +1,38 @@
-import { createBrowserRouter } from "react-router-dom";
-import RootLayout from "../RootLayout";
+import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from '../RootLayout';
+import Home from '../pages/Home';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
+import Calendar from '../pages/Calendar';
+import Day from '../pages/Day';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
-      // {
-      //   index: true,
-      //   element: <Home />,
-      // },
-      // {
-      //   path: "/posts",
-      //   element: <PostList />,
-      // },
+      {
+        index: true,
+        element: <Home />,
+      },
     ],
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/calendar',
+    element: <Calendar />,
+  },
+  {
+    path: '/day',
+    element: <Day />,
+    children: [],
   },
 ]);
 
