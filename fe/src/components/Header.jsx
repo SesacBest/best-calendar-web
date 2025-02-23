3ba1bc7544bb1f0125e0478fa77e4bd9bf42b9f8
kska01 <insideka01@gmail.com>
2025-02-23 22:44:12 +0900

    [BCW-10] feat: 회원가입, 로그인 버튼의 이동기능 구현

--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <>
       <section className="flex justify-between">
         <div>S-Calendar</div>
-        <section className="flex gap-8">
-          <div>회원가입</div>
-          <div>로그인</div>
-        </section>
+        <nav className="flex gap-8">
+          <Link to="/signup">회원가입</Link>
+          <Link to="/login">로그인</Link>
+        </nav>
       </section>
     </>
   );
