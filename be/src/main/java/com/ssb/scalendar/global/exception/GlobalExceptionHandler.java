52e0d47c1874f0b4a326b998475bbd98c15f314f
bjh0499 <j9603h@gmail.com>
2025-02-24 14:17:29 +0900

    [BCW-12] fix: 예외 처리 클래스에서 누락된 annotation 추가

--- be/src/main/java/com/ssb/scalendar/global/exception/GlobalExceptionHandler.java
+++ be/src/main/java/com/ssb/scalendar/global/exception/GlobalExceptionHandler.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleCustomNotFound(ResourceNotFoundException ex) {
