6bdf038f47e5bf37656f44f340c2f2a31653d460
hisunny1007 <hisunny1007@gmail.com>
2025-02-24 20:43:03 +0900

    [BCW-13] feat: 로그인 페이지 디자인 CSS 적용 - 테일윈드 사용

--- fe/src/api/axios.js
+++ fe/src/api/axios.js
@@ -4,4 +4,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
+
+
 export default api;
--- fe/src/pages/Login.jsx
+++ fe/src/pages/Login.jsx
@@ -5,12 +5,11 @@ import { useDispatch } from 'react-redux';
 import { login } from '../store/slices/authSlice';
 
 export default function Login() {
-  
   const [formData, setFormData] = useState({
     username: '',
     password: '',
   });
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -19,69 +18,80 @@ export default function Login() {
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
-      [name] : value,
+      [name]: value,
     }));
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError("");
+    setError('');
     setIsLoading(true);
 
     try {
       const response = await authApi.login(formData);
       const data = response.data;
 
-      const {token} = data.data;
+      const { token } = data.data;
       dispatch(login(token));
       navigate('/calendar');
-
     } catch (err) {
-      setError(err.message || "로그인 중 오류가 발생했습니다.");
+      setError(err.message || '로그인 중 오류가 발생했습니다.');
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <div className="flex flex-col gap-10 m-40 text-center">
-      <h1 className="text-5xl font-semibold">S-Calendar</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username"></label>
-          <input
-          id="username"
-          name="username"
-          required
-          placeholder="이메일"
-          value={formData.username}
-          onChange={handleFormInput}
-          />
-        </div>
+    <div className="flex items-center flex-col gap-10 m-40 text-center">
+      <Link to="/">
+        <h1 className="text-5xl font-semibold">S-Calendar</h1>
+      </Link>
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-4 w-100">
 
-        <div>
-          <label htmlFor="password"></label>
-          <input
-          type="password"
-          id="password"
-          name="password"
-          required
-          placeholder="비밀번호"
-          value={formData.password}
-          onChange={handleFormInput}
-          />
-        </div>
+            <div className="space-y-2">
+              <label htmlFor="username"></label>
+              <input
+                className="w-full py-2.5 text-gray-900 bg-transparent border-0 border-b border-gray-300 
+                  focus:outline-none focus:border-gray-900 focus:ring-0 text-sm"
+                id="username"
+                name="username"
+                required
+                placeholder="이메일"
+                value={formData.username}
+                onChange={handleFormInput}
+              />
+            </div>
 
-        <button
-        type="submit">
-        로그인
-        </button>
+            <div className="space-y-2">
+              <label htmlFor="password"></label>
+              <input
+                className="w-full py-2.5 text-gray-900 bg-transparent border-0 border-b border-gray-300 
+                  focus:outline-none focus:border-gray-900 focus:ring-0 text-sm"
+                type="password"
+                id="password"
+                name="password"
+                required
+                placeholder="비밀번호"
+                value={formData.password}
+                onChange={handleFormInput}
+              />
+            </div>
+            
+            <button
+              className="w-full py-2 px-4 bg-[#2799C3] text-white rounded-md text-sm
+                hover:bg-[#2182a7] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#2799C3]"
+              type="submit"
+            >
+              {isLoading ? '처리중...' : '로그인'}
+            </button>
 
-        <nav>
-          <Link to="/signup">회원가입</Link>
-        </nav>
+            <nav className="text-center text-sm text-gray-600">
+              <Link to="/signup">회원가입</Link>
+            </nav>
 
-      </form>
+          </div>
+        </form>
     </div>
   );
 }
