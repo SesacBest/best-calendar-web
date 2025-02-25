09f7f4f47860b12f0ee120b72a28a5724ee3175d
hisunny1007 <hisunny1007@gmail.com>
2025-02-25 16:29:51 +0900

    [BCW-31] fix: 로그인 시 에러 메세지 상태 관리 로직 개선 - 에러 메세지 수정 및 색상 변경

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useNavigate } from 'react-router-dom';
 import router from './router';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import AuthProvider from './AuthProvider';
+import RootLayout from './RootLayout';
 
 function App() {
   return (
