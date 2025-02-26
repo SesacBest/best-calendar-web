7cec29f7227361e39fc1095682bd209d63f6db15
KoHo512 <sesac.yw@gmail.com>
2025-02-26 22:31:30 +0900

    [BCW-24] feat: 일정 생성 api 구현

--- be/src/main/java/com/ssb/scalendar/domain/schedule/controller/ScheduleController.java
+++ be/src/main/java/com/ssb/scalendar/domain/schedule/controller/ScheduleController.java
@@ -1,11 +1,14 @@
 package com.ssb.scalendar.domain.schedule.controller;
 
+import com.ssb.scalendar.domain.schedule.dto.request.ScheduleCreateRequestDto;
 import com.ssb.scalendar.domain.schedule.service.ScheduleService;
+import com.ssb.scalendar.global.response.ApiResponse;
+import com.ssb.scalendar.global.security.jwt.JwtTokenProvider;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calendar")
@@ -13,5 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class ScheduleController {
     private final ScheduleService scheduleService;
+    private final JwtTokenProvider jwtTokenProvider;
 
+    @PostMapping("/schedules")
+    public ResponseEntity<ApiResponse<Object>> createSchedule(
+            @Valid @RequestBody ScheduleCreateRequestDto requestDto,
+            @RequestHeader("Authorization") String authorization
+            ) {
+
+        String jwt = authorization.replace("Bearer ", "");
+        Long userId = jwtTokenProvider.getUserId(jwt);
+
+        scheduleService.createSchedule(requestDto, userId);
+
+        return ResponseEntity.ok(ApiResponse.ok(
+                "일정이 생성되었습니다.", "OK", null
+                ));
+    }
 }
--- be/src/main/java/com/ssb/scalendar/domain/schedule/service/ScheduleService.java
+++ be/src/main/java/com/ssb/scalendar/domain/schedule/service/ScheduleService.java
@@ -1,6 +1,10 @@
 package com.ssb.scalendar.domain.schedule.service;
 
+import com.ssb.scalendar.domain.schedule.dto.request.ScheduleCreateRequestDto;
 import com.ssb.scalendar.domain.schedule.repository.ScheduleRepository;
+import com.ssb.scalendar.domain.user.entity.User;
+import com.ssb.scalendar.domain.user.repository.UserRepository;
+import com.ssb.scalendar.global.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void createSchedule(ScheduleCreateRequestDto requestDto, Long userId) {
+        User user = userRepository.findById(userId)
+                        .orElseThrow(() -> new ResourceNotFoundException());
+
+        scheduleRepository.save(requestDto.toEntity(user));
+    }
 }
