30c6cdc3413965ba28e08ed71d512cf7beaacf46
kska01 <insideka01@gmail.com>
2025-02-27 13:52:13 +0900

    [BCW-50] feat: 로그인에 따른 로그인/로그아웃 교체 기능 및 로그아웃 시 랜딩페이지로 이동 구현
    - header size, text-xl로 변경

--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../store/slices/authSlice';
+import { useDispatch } from 'react-redux';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const token = localStorage.getItem('token');
+  console.log(token);
+  let isLoginHidden;
+  let isLogoutHidden;
+
+  if (token === null) {
+    isLoginHidden = false;
+    isLogoutHidden = true;
+  } else {
+    isLoginHidden = true;
+    isLogoutHidden = false;
+  }
+
+  const handleOnLogoutClick = () => {
+    dispatch(logout());
+    navigate('/');
+  };
+
   return (
     <>
-      <section className="flex justify-between">
+      <section className="flex justify-between text-xl">
         <div>S-Calendar</div>
         <nav className="flex gap-8">
           <Link to="/signup">회원가입</Link>
-          <Link to="/login">로그인</Link>
+          <Link to="/login" hidden={isLoginHidden}>
+            로그인
+          </Link>
+          <Link onClick={handleOnLogoutClick} hidden={isLogoutHidden}>
+            로그아웃
+          </Link>
         </nav>
       </section>
     </>
