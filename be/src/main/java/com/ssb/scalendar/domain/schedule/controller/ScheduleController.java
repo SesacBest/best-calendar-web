29204c763c9ac072b3bfd4e25d1e1b9046ffc63c
KoHo512 <sesac.yw@gmail.com>
2025-02-27 14:59:35 +0900

    [BCW-24] feat: 일정 조회 by month api 구현 및 @AuthenticationPrincipal 사용하도록 리팩토링
    - 토큰 만들 때 SecurityContextHolder에 인증 정보를 설정해서 @AuthenticationPrincipal 사용할 수 있어서 코드 리팩토링.

--- be/src/main/java/com/ssb/scalendar/domain/schedule/controller/ScheduleController.java
+++ be/src/main/java/com/ssb/scalendar/domain/schedule/controller/ScheduleController.java
@@ -1,14 +1,24 @@
 package com.ssb.scalendar.domain.schedule.controller;
 
 import com.ssb.scalendar.domain.schedule.dto.request.ScheduleCreateRequestDto;
+import com.ssb.scalendar.domain.schedule.dto.response.MonthlyScheduleResponseDto;
+import com.ssb.scalendar.domain.schedule.entity.Schedule;
+import com.ssb.scalendar.domain.schedule.repository.DailyCount;
 import com.ssb.scalendar.domain.schedule.service.ScheduleService;
+import com.ssb.scalendar.domain.user.entity.User;
 import com.ssb.scalendar.global.response.ApiResponse;
 import com.ssb.scalendar.global.security.jwt.JwtTokenProvider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.YearMonth;
+import java.util.List;
 
 @RestController
 @RequestMapping("/calendar")
@@ -20,15 +30,22 @@ public class ScheduleController {
     @PostMapping("/schedules")
     public ResponseEntity<ApiResponse<Object>> createSchedule(
             @Valid @RequestBody ScheduleCreateRequestDto requestDto,
-            @RequestHeader("Authorization") String authorizationHeader) {
+            @AuthenticationPrincipal User user) {
 
-        String token = authorizationHeader.replace("Bearer ", "");
-        Long userId = jwtTokenProvider.getUserId(token);
-
-        scheduleService.createSchedule(requestDto, userId);
+        scheduleService.createSchedule(requestDto, user);
 
         return ResponseEntity.ok(ApiResponse.ok(
                 "일정이 생성되었습니다.", "OK", null
                 ));
+    }
+
+    @GetMapping("/monthly-schedules")
+    public ResponseEntity<ApiResponse<List<MonthlyScheduleResponseDto>>> readSchedulesByMonth(
+            @RequestParam("month") YearMonth month,
+            @AuthenticationPrincipal User user) {
+        
+        return ResponseEntity.ok(ApiResponse.ok(
+                "일정 조회에 성공했습니다.", "OK", scheduleService.readSchedulesByMonth(month, user)
+        ));
     }
 }
--- be/src/main/java/com/ssb/scalendar/domain/schedule/service/ScheduleService.java
+++ be/src/main/java/com/ssb/scalendar/domain/schedule/service/ScheduleService.java
@@ -1,6 +1,7 @@
 package com.ssb.scalendar.domain.schedule.service;
 
 import com.ssb.scalendar.domain.schedule.dto.request.ScheduleCreateRequestDto;
+import com.ssb.scalendar.domain.schedule.dto.response.MonthlyScheduleResponseDto;
 import com.ssb.scalendar.domain.schedule.repository.ScheduleRepository;
 import com.ssb.scalendar.domain.user.entity.User;
 import com.ssb.scalendar.domain.user.repository.UserRepository;
@@ -8,6 +9,10 @@ import com.ssb.scalendar.global.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,10 +22,17 @@ public class ScheduleService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void createSchedule(ScheduleCreateRequestDto requestDto, Long userId) {
-        User user = userRepository.findById(userId)
-                        .orElseThrow(() -> new ResourceNotFoundException());
-
+    public void createSchedule(ScheduleCreateRequestDto requestDto, User user) {
         scheduleRepository.save(requestDto.toEntity(user));
+    }
+
+    public List<MonthlyScheduleResponseDto> readSchedulesByMonth(YearMonth month, User user) {
+
+        LocalDate startDate = month.atDay(1);
+        LocalDate endDate = month.atEndOfMonth();
+
+        return scheduleRepository.countSchedulesByUserAndSelectedDateBetween(user, startDate, endDate).stream()
+                .map(MonthlyScheduleResponseDto::from)
+                .toList();
     }
 }
