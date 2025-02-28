61b75f4511d52a5da69e18349da026e2c7a0546c
kska01 <insideka01@gmail.com>
2025-02-28 20:20:19 +0900

    [BCW-25] feat: 월별 조회, 일별 조회, 삭제 api 구현

--- be/src/main/java/com/ssb/scalendar/domain/task/controller/TaskController.java
+++ be/src/main/java/com/ssb/scalendar/domain/task/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.ssb.scalendar.domain.task.controller;
 
 import com.ssb.scalendar.domain.task.dto.request.TaskCreateRequestDto;
+import com.ssb.scalendar.domain.task.dto.response.MonthlyTaskResponseDto;
+import com.ssb.scalendar.domain.task.dto.response.TaskResponseDto;
 import com.ssb.scalendar.domain.task.service.TaskService;
 import com.ssb.scalendar.domain.user.entity.User;
 import com.ssb.scalendar.global.response.ApiResponse;
@@ -8,10 +10,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
 
 @RestController
 @RequestMapping("/calendar")
@@ -21,12 +24,43 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping("/tasks")
-    public ResponseEntity<ApiResponse<Object>> createSchedule(
+    public ResponseEntity<ApiResponse<Object>> createTask(
             @Valid @RequestBody TaskCreateRequestDto requestDto,
             @AuthenticationPrincipal User user) {
 
         taskService.createTask(requestDto, user);
 
-        return ResponseEntity.ok(ApiResponse.ok("일정이 생성되었습니다.", "OK", null));
+        return ResponseEntity.ok(ApiResponse.ok("할 일이 생성되었습니다.", "OK", null));
+    }
+
+    @GetMapping("/monthly-tasks")
+    public ResponseEntity<ApiResponse<List<MonthlyTaskResponseDto>>> findAllByMonth(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestParam YearMonth month
+    ) {
+        return ResponseEntity.ok(ApiResponse.ok(
+                "할 일 조회에 성공했습니다.",
+                "OK",
+                taskService.findAllByMonth(user, month)
+        ));
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<ApiResponse<List<TaskResponseDto>>> getTasksByDate(
+            @Valid @RequestParam LocalDate date,
+            @AuthenticationPrincipal User user
+    ) {
+        return ResponseEntity.ok(ApiResponse.ok(
+                "할 일 조회에 성공했습니다.",
+                "OK",
+                taskService.readTasksByDate(date, user))
+        );
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<ApiResponse<Object>> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+
+        return ResponseEntity.ok(ApiResponse.ok("할 일이 삭제되었습니다.", "OK", null));
     }
 }
--- be/src/main/java/com/ssb/scalendar/domain/task/service/TaskService.java
+++ be/src/main/java/com/ssb/scalendar/domain/task/service/TaskService.java
@@ -1,11 +1,19 @@
 package com.ssb.scalendar.domain.task.service;
 
 import com.ssb.scalendar.domain.task.dto.request.TaskCreateRequestDto;
+import com.ssb.scalendar.domain.task.dto.response.MonthlyTaskResponseDto;
+import com.ssb.scalendar.domain.task.dto.response.TaskResponseDto;
+import com.ssb.scalendar.domain.task.entity.Task;
 import com.ssb.scalendar.domain.task.repository.TaskRepository;
 import com.ssb.scalendar.domain.user.entity.User;
+import com.ssb.scalendar.global.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -14,8 +22,53 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
 
+
     @Transactional
     public void createTask(TaskCreateRequestDto requestDto, User user) {
         taskRepository.save(requestDto.toEntity(user));
     }
+
+//    public List<MonthlyTaskResponseDto> preFindAllByMonth(User user, String month) {
+//        // 입력 문자열을 파싱하기 위한 포맷터를 정의.
+//        DateTimeFormatter ymFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
+//
+//        // YearMonth 객체로 변환.
+//        YearMonth yearMonth = YearMonth.parse(month, ymFormatter);
+//
+//        // 해당 월의 첫째 날로 startDate를 생성.
+//        LocalDate startDate = yearMonth.atDay(1);
+//        // 다음 달의 첫째 날로 endDate를 생성.
+//        LocalDate endDate = yearMonth.plusMonths(1).atDay(1);
+//
+//        // 원하는 형식으로 출력하기 위해 포맷터를 설정.
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//
+//        return findAllByMonth(user, startDate, endDate);
+//
+//    }
+
+    public List<MonthlyTaskResponseDto> findAllByMonth(User user, YearMonth month) {
+        LocalDate startDate = month.atDay(1);
+        LocalDate endDate = month.atEndOfMonth();
+
+        return taskRepository.findAllByMonth(user, startDate, endDate).stream()
+                .map(MonthlyTaskResponseDto::from)
+                .toList();
+    }
+
+
+    public List<TaskResponseDto> readTasksByDate(LocalDate date, User user) {
+        return taskRepository.findAllByUserAndSelectedDate(user, date).stream()
+                .map(TaskResponseDto::from)
+                .toList();
+    }
+
+    @Transactional
+    public void deleteTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+
+        taskRepository.delete(task);
+    }
+
 }
