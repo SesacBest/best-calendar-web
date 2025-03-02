32b6b446408345ca5a0631c4e796c58bfe669e25
rkdrudgns0412 <rkdrudgns8233@gmail.com>
2025-03-02 16:45:24 +0900

    [BCW-19] feat: 일정 생성 페이지 디자인 추가

--- fe/src/components/day/ScheduleCreate.jsx
+++ fe/src/components/day/ScheduleCreate.jsx
@@ -41,25 +41,33 @@ export default function ScheduleCreate() {
   // }, [isLoading]);
 
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <input
+    <form action="" onSubmit={handleSubmit} className='flex flex-col items-center mt-40 gap-35'>
+      <div className='flex flex-col gap-7'>
+        <label for="content" className='font-mono text-xl'>content</label>
+        <input
         type="text"
-        className="test"
+        className="w-230 p-2 border-b-4 rounded-lg shadow-sm focus:outline-none focus:border-violet-600 bg-gray-200 text-gray-700"
         id="content"
         name="content"
         value={formData.content}
         onChange={handleChange}
         required
-      />
-      <input
+        />
+      </div>
+      <div className='flex flex-col gap-7'>
+        <label for="schduleTime" className='font-mono text-xl'>scheduleTime</label>
+        <input
         type="time"
-        className="w-40 border border-gray-300 rounded px-2 py-1 bg-black text-white"
+        // className="w-40 border border-gray-300 rounded px-2 py-1 bg-black text-white"
+        className='w-230 p-2 border-b-4 rounded-lg shadow-sm focus:outline-none focus:border-violet-600 bg-gray-200 text-gray-700'
         id="scheduleTime"
         name="scheduleTime"
         value={formData.scheduleTime}
         onChange={handleChange}
-      />
-      <button>{isLoading ? '생성중...' : '생성'}</button>
+        />
+        
+      </div>
+      <button className='px-3 py-2 rounded-lg text-gray-500 hover:bg-violet-600 hover:text-white border w-15 absolute right-45 bottom-5'>{isLoading ? '생성중' : '생성'}</button>
     </form>
   );
 }
