4696d5696dd99d59f4445d76c3bef09b205e7451
hisunny1007 <hisunny1007@gmail.com>
2025-03-03 19:25:10 +0900

    [BCW-61] refactor: 카카오 로그인 리다이렉트 수정

--- fe/src/pages/KakaoRedirect.jsx
+++ fe/src/pages/KakaoRedirect.jsx
@@ -1,25 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import authApi from '../api/authApi';
 
 export default function KakaoRedirect() {
-  // http://localhost:5173/login/oauth/callback/kakao?code=uUEiy82-ITfC4KT3wUOWeugcSOrDXiOMx0gIrRRVqNEHVFWEGNRABwAAAAQKKcjZAAABlVtEkL3SDh85zpcCzQ
   // ?code=인가코드 / 파라미터로 넘어온 인가코드를 가져와야 함
   const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get("code");
+    // 카카오 로그인 후 리다이렉트 시 URL에 있는 인가코드(인증코드) 가져오기
+    const code = new URL(window.location.href).searchParams.get('code');
     console.log(code); // 인가코드
-    console.log("테스트");
 
+    const getKakaoToken = async (code) => {
+      setIsLoading(true); // 로딩 상태 설정
+      try {
+        const response = await authApi.kakaoLogin(code);
+        console.log(response);
+        navigate('/calendar');
+      } catch (err) {
+        console.log('카카오 로그인 처리 중 오류 발생:', err);
+        // setError(err.response.data.message)
+        setError('로그인 처리 중 오류가 발생했습니다.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  }, []);
+    if (code) {
+      getKakaoToken(code);
+    }
+  }, [navigate]);
 
-
-
-  return (
-    <div>
-      <p>로그인 중입니다.</p>
-      <p>잠시만 기다려 주세요.</p>
-    </div>
-  )
+  return <div>로그인 중입니다.</div>;
 }
