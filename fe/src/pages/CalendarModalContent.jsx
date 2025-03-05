9999f9b3186b74f046282abf6e0e5fd8550a011f
bjh0499 <j9603h@gmail.com>
2025-03-05 12:52:36 +0900

    [BCW-62] fix: 달력 설정 modal 부분 스타일 개선

--- fe/src/pages/CalendarModalContent.jsx
+++ fe/src/pages/CalendarModalContent.jsx
@@ -6,6 +6,24 @@ export default function CalendarModalContent({
   todayColorsArray,
   holidayColorsArray,
 }) {
+  const dataButtonColorsArray = [
+    'border-green-300 text-green-300',
+    'border-red-300 text-red-300',
+    'border-blue-300 text-blue-300',
+    'border-yellow-300 text-yellow-300',
+    'border-orange-300 text-orange-300',
+  ];
+  const todayButtonColorsArray = [
+    'border-black text-black',
+    'border-primary text-primary',
+    'border-secondary text-secondary',
+  ];
+  const holidayButtonColorsArray = [
+    'border-red-300 text-red-300',
+    'border-blue-300 text-blue-300',
+    'border-green-300 text-green-300',
+  ];
+
   const changeTodayColor = () => {
     const nextIndex = (calendarOption.todayColorIndex + 1) % todayColorsArray.length;
     document.documentElement.style.setProperty('--fc-today-bg-color', todayColorsArray[nextIndex]);
@@ -22,96 +40,136 @@ export default function CalendarModalContent({
     }));
   };
 
+  const linkStyle =
+    'px-3 py-2 mr-2 rounded-lg hover:bg-primary hover:text-white border cursor-pointer';
+
   return (
     <div className="w-full h-full bg-black/75 absolute top-0 left-0 z-3 flex" onClick={onClose}>
       <div
-        className="w-150 h-100 bg-white mx-auto my-auto p-5 flex flex-col justify-center items-center"
+        className="w-150 h-100 bg-white mx-auto my-auto flex flex-col justify-center items-center"
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-          }}
-        >
-          <div>
-            <label htmlFor="todayColor">오늘 강조 색상</label>
-            <input
-              type="button"
-              name="todayColor"
-              className="cursor-pointer"
-              value="변경"
-              onClick={() => changeTodayColor()}
-            />
-          </div>
-          <div>
-            <label htmlFor="dataColor">일정 색상</label>
-            <input
-              type="button"
-              name="dataColor"
-              className="cursor-pointer"
-              value="변경"
-              onClick={() =>
-                changeOption(
-                  'dataColorIndex',
-                  (calendarOption.dataColorIndex + 1) % dataColorsArray.length,
-                )
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="sundayColor">일요일 색상</label>
-            <input
-              type="button"
-              name="sundayColor"
-              className="cursor-pointer"
-              value="변경"
-              onClick={() =>
-                changeOption(
-                  'sundayColorIndex',
-                  (calendarOption.sundayColorIndex + 1) % holidayColorsArray.length,
-                )
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="saturdayColor">토요일 색상</label>
-            <input
-              type="button"
-              name="saturdayColor"
-              className="cursor-pointer"
-              value="변경"
-              onClick={() =>
-                changeOption(
-                  'saturdayColorIndex',
-                  (calendarOption.saturdayColorIndex + 1) % holidayColorsArray.length,
-                )
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="firstDayOfWeek">주 시작 요일</label>
-            <input
-              type="button"
-              name="firstDayOfWeek"
-              className="cursor-pointer"
-              value="일요일"
-              onClick={() => changeOption('firstDayOfWeek', 0)}
-            />
-            <input
-              type="button"
-              name="firstDayOfWeek"
-              className="cursor-pointer"
-              value="월요일"
-              onClick={() => changeOption('firstDayOfWeek', 1)}
-            />
-          </div>
-          <button className="cursor-pointer" onClick={onClose}>
-            확인
-          </button>
-        </form>
+        <div className="w-70 h-100 mx-auto my-auto flex flex-col justify-center items-center">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+            }}
+          >
+            <div className="w-70 my-5 flex flex-row justify-between items-center">
+              <div className="flex">
+                <label className="text-xl" htmlFor="todayColor">
+                  오늘 강조 색상
+                </label>
+              </div>
+              <div className="flex">
+                <input
+                  type="button"
+                  name="todayColor"
+                  className={`${linkStyle} ${todayButtonColorsArray[calendarOption.todayColorIndex]}`}
+                  value="변경"
+                  onClick={() => changeTodayColor()}
+                />
+              </div>
+            </div>
+            <div className="w-70 my-5 flex flex-row justify-between items-center">
+              <div className="flex">
+                <label className="text-xl" htmlFor="dataColor">
+                  일정 색상
+                </label>
+              </div>
+              <div className="flex">
+                <input
+                  type="button"
+                  name="dataColor"
+                  className={`${linkStyle} ${dataButtonColorsArray[calendarOption.dataColorIndex]}`}
+                  value="변경"
+                  onClick={() =>
+                    changeOption(
+                      'dataColorIndex',
+                      (calendarOption.dataColorIndex + 1) % dataColorsArray.length,
+                    )
+                  }
+                />
+              </div>
+            </div>
+            <div className="w-70 my-5 flex flex-row justify-between items-center">
+              <div className="flex">
+                <label className="text-xl" htmlFor="sundayColor">
+                  일요일 색상
+                </label>
+              </div>
+              <div className="flex">
+                <input
+                  type="button"
+                  name="sundayColor"
+                  className={`${linkStyle} ${holidayButtonColorsArray[calendarOption.sundayColorIndex]}`}
+                  value="변경"
+                  onClick={() =>
+                    changeOption(
+                      'sundayColorIndex',
+                      (calendarOption.sundayColorIndex + 1) % holidayColorsArray.length,
+                    )
+                  }
+                />
+              </div>
+            </div>
+            <div className="w-70 my-5 flex flex-row justify-between items-center">
+              <div className="flex">
+                <label className="text-xl" htmlFor="saturdayColor">
+                  토요일 색상
+                </label>
+              </div>
+              <div className="flex">
+                <input
+                  type="button"
+                  name="saturdayColor"
+                  className={`${linkStyle} ${holidayButtonColorsArray[calendarOption.saturdayColorIndex]}`}
+                  value="변경"
+                  onClick={() =>
+                    changeOption(
+                      'saturdayColorIndex',
+                      (calendarOption.saturdayColorIndex + 1) % holidayColorsArray.length,
+                    )
+                  }
+                />
+              </div>
+            </div>
+            <div className="w-70 my-5 flex flex-row justify-between items-center">
+              <div className="flex">
+                <label className="text-xl" htmlFor="firstDayOfWeek">
+                  주 시작 요일
+                </label>
+              </div>
+              <div className="flex">
+                <input
+                  type="button"
+                  name="firstDayOfWeek"
+                  className={`${linkStyle} ${calendarOption.firstDayOfWeek ? '' : 'border-primary text-primary'}`}
+                  value="일요일"
+                  onClick={() => changeOption('firstDayOfWeek', 0)}
+                />
+                <input
+                  type="button"
+                  name="firstDayOfWeek"
+                  className={`${linkStyle} ${calendarOption.firstDayOfWeek ? 'border-primary text-primary' : ''}`}
+                  value="월요일"
+                  onClick={() => changeOption('firstDayOfWeek', 1)}
+                />
+              </div>
+            </div>
+            <div className="w-70 my-5 flex flex-row justify-center items-center">
+              <button
+                className="px-3 py-2 mr-2 rounded-lg bg-primary text-white border cursor-pointer"
+                onClick={onClose}
+              >
+                확인
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
