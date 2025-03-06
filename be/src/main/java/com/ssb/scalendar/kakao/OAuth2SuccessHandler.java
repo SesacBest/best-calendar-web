661443f1b059ee5c67d38230fa0f5b1acace10df
hisunny1007 <hisunny1007@gmail.com>
2025-03-06 10:06:15 +0900

    [BCW-64] chore: 환경변수 설정 (백엔드)
    - 프론트 환경변수는 삭제함

--- be/src/main/java/com/ssb/scalendar/kakao/OAuth2SuccessHandler.java
+++ be/src/main/java/com/ssb/scalendar/kakao/OAuth2SuccessHandler.java
@@ -5,6 +5,7 @@ import com.ssb.scalendar.global.security.jwt.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     // OAuth2SuccessHandler : 로그인 성공 후 처리 로직 (리다이렉트 URL, 토큰 생성 등 담당)
 
     private final JwtTokenProvider jwtTokenProvider;
+    @Value("${origin}")
+    private String origin;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -32,6 +35,6 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         // JWT를 프론트엔드로 전달
         response.addHeader("Authorization", "Bearer " + token);
-        getRedirectStrategy().sendRedirect(request, response, "http://localhost:5173/login-success?token=" + token);
+        getRedirectStrategy().sendRedirect(request, response, origin + "/login-success?token=" + token);
     }
 }
